3d88aecea842d25da9a17720782f19ba6dd1436f
Vladimir Stojanovic <st.vladas@hotmail.com>
2021-10-12 10:26:53 +0200

    styling, min-width, remove semi from js

--- index.js
+++ index.js
@@ -1,118 +1,116 @@
 class Calculator {
     constructor(previousNumberTextElement, currentNumberTextElement) {
-        this.previousNumberTextElement = previousNumberTextElement;
-        this.currentNumberTextElement = currentNumberTextElement;
-        this.reset();
+        this.previousNumberTextElement = previousNumberTextElement
+        this.currentNumberTextElement = currentNumberTextElement
+        this.reset()
     }
     //clear screen
     reset() {
-        this.currentNumber = '';
-        this.previousNumber = '';
-        this.operation = undefined;
+        this.currentNumber = ''
+        this.previousNumber = ''
+        this.operation = undefined
     }
     //backspace
     delete() {
-        this.currentNumber = this.currentNumber.toString().slice(0, -1);
+        this.currentNumber = this.currentNumber.toString().slice(0, -1)
     }
     //append number to screen
     addNumberToScreen(number) {
         //prevent '.' multiple times
-        if (number === '.' && this.currentNumber.includes('.')) return;
-        this.currentNumber = this.currentNumber.toString() + number.toString();
+        if (number === '.' && this.currentNumber.includes('.')) return
+        this.currentNumber = this.currentNumber.toString() + number.toString()
     }
 
     chooseOperation(operation) {
         //prevent opration if there is no number
-        if (this.currentNumber === '') return;
+        if (this.currentNumber === '') return
         //do calculation if operation button is clicked
         if (this.currentNumber != '') {
-            this.calculate();
+            this.calculate()
         }
-        this.operation = operation;
-        this.previousNumber = this.currentNumber;
-        this.currentNumber = '';
+        this.operation = operation
+        this.previousNumber = this.currentNumber
+        this.currentNumber = ''
     }
 
     calculate() {
-        let calculation;
+        let calculation
 
-        const prev = parseFloat(this.previousNumber);
-        const current = parseFloat(this.currentNumber);
+        const prev = parseFloat(this.previousNumber)
+        const current = parseFloat(this.currentNumber)
         //check for numbers
-        if (isNaN(prev) || isNaN(current)) return;
+        if (isNaN(prev) || isNaN(current)) return
         switch (this.operation) {
             case '+':
-                calculation = prev + current;
-                break;
+                calculation = prev + current
+                break
             case '-':
-                calculation = prev - current;
-                break;
+                calculation = prev - current
+                break
             case '*':
-                calculation = prev * current;
-                break;
+                calculation = prev * current
+                break
             case '/':
-                calculation = prev / current;
-                break;
+                calculation = prev / current
+                break
             default:
-                return;
+                return
         }
-        this.currentNumber = calculation;
-        this.operation = undefined;
-        this.previousNumber = '';
+        this.currentNumber = calculation
+        this.operation = undefined
+        this.previousNumber = ''
     }
 
     updateScreen() {
-        currentNumberTextElement.innerText = this.currentNumber;
+        currentNumberTextElement.innerText = this.currentNumber
         if (this.operation != null) {
-            previousNumberTextElement.innerText = `${this.previousNumber} ${this.operation} `;
+            previousNumberTextElement.innerText = `${this.previousNumber} ${this.operation}`
         } else {
-            previousNumberTextElement.innerText = '';
+            previousNumberTextElement.innerText = ''
         }
     }
 }
 
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const deleteButton = document.querySelector('[data-delete]');
-const resetButton = document.querySelector('[data-reset]');
-const equalButton = document.querySelector('[data-equal]');
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const deleteButton = document.querySelector('[data-delete]')
+const resetButton = document.querySelector('[data-reset]')
+const equalButton = document.querySelector('[data-equal]')
 const previousNumberTextElement = document.querySelector(
-    '[data-previous-number]'
-);
-const currentNumberTextElement = document.querySelector(
-    '[data-current-number]'
-);
+    '[data-previous-number]',
+)
+const currentNumberTextElement = document.querySelector('[data-current-number]')
 
 const calculator = new Calculator(
     previousNumberTextElement,
-    currentNumberTextElement
-);
+    currentNumberTextElement,
+)
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        calculator.addNumberToScreen(button.innerText);
-        calculator.updateScreen();
-    });
-});
+        calculator.addNumberToScreen(button.innerText)
+        calculator.updateScreen()
+    })
+})
 
 operationButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText);
-        calculator.updateScreen();
-    });
-});
+        calculator.chooseOperation(button.innerText)
+        calculator.updateScreen()
+    })
+})
 
 equalButton.addEventListener('click', (button) => {
-    calculator.calculate();
-    calculator.updateScreen();
-});
+    calculator.calculate()
+    calculator.updateScreen()
+})
 
 resetButton.addEventListener('click', (button) => {
-    calculator.reset();
-    calculator.updateScreen();
-});
+    calculator.reset()
+    calculator.updateScreen()
+})
 
 deleteButton.addEventListener('click', (button) => {
-    calculator.delete();
-    calculator.updateScreen();
-});
+    calculator.delete()
+    calculator.updateScreen()
+})
--- theme.js
+++ theme.js
@@ -1,61 +1,61 @@
 const userPrefersDark =
     window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches;
+    window.matchMedia('(prefers-color-scheme: dark)').matches
 
 if (userPrefersDark) {
-    document.body.classList.add('dark');
-    document.getElementById('slider').value = 1;
+    document.body.classList.add('dark')
+    document.getElementById('slider').value = 1
 } else {
-    document.body.classList.add('light');
-    document.getElementById('slider').value = 2;
+    document.body.classList.add('light')
+    document.getElementById('slider').value = 2
 }
 
 function themeChanger() {
-    var slider = document.getElementById('slider').value;
-    var body = document.body;
+    var slider = document.getElementById('slider').value
+    var body = document.body
     switch (slider) {
         case '1':
-            body.classList.add('dark');
-            window.localStorage.setItem('theme', 'dark');
-            body.classList.remove('light');
-            body.classList.remove('purple');
-            break;
+            body.classList.add('dark')
+            window.localStorage.setItem('theme', 'dark')
+            body.classList.remove('light')
+            body.classList.remove('purple')
+            break
         case '2':
-            body.classList.remove('dark');
-            body.classList.add('light');
-            window.localStorage.setItem('theme', 'light');
-            body.classList.remove('purple');
-            break;
+            body.classList.remove('dark')
+            body.classList.add('light')
+            window.localStorage.setItem('theme', 'light')
+            body.classList.remove('purple')
+            break
         case '3':
-            body.classList.add('purple');
-            window.localStorage.setItem('theme', 'purple');
-            body.classList.remove('dark');
-            body.classList.remove('light');
-            break;
+            body.classList.add('purple')
+            window.localStorage.setItem('theme', 'purple')
+            body.classList.remove('dark')
+            body.classList.remove('light')
+            break
         default:
-            break;
+            break
     }
 }
 
 document.getElementById('slider').onchange = function () {
-    themeChanger();
-};
+    themeChanger()
+}
 
-window.onload = checkTheme();
+window.onload = checkTheme()
 
 function checkTheme() {
-    let localStorageTheme = localStorage.getItem('theme');
+    let localStorageTheme = localStorage.getItem('theme')
 
     if (localStorageTheme != null && localStorageTheme === 'dark') {
-        document.body.className = localStorageTheme;
-        document.getElementById('slider').value = 1;
+        document.body.className = localStorageTheme
+        document.getElementById('slider').value = 1
     }
     if (localStorageTheme != null && localStorageTheme === 'light') {
-        document.body.className = localStorageTheme;
-        document.getElementById('slider').value = 2;
+        document.body.className = localStorageTheme
+        document.getElementById('slider').value = 2
     }
     if (localStorageTheme != null && localStorageTheme === 'purple') {
-        document.body.className = localStorageTheme;
-        document.getElementById('slider').value = 3;
+        document.body.className = localStorageTheme
+        document.getElementById('slider').value = 3
     }
 }
